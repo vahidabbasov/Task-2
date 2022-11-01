6c8b2650fb0556960fb026adba64e4524eb32940
vahidabbasov <tu7hl7kxu@code.edu.az>
2022-11-02 01:16:19 +0400

    Added C# & Js tasks

---
+++ Tasks C#/Task1&2/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿#region Task1 
+//int[] numbers = { 12, 321, 23, 8, 239 };
+//int min = numbers[0];
+//for (int i = 1; i < numbers.Length; i++)
+//{
+//	if (numbers[i]<min)
+//	{
+//		min = numbers[i];	
+//	}
+//}
+//Console.WriteLine(min);
+#endregion
+
+#region Task2
+//int[] numbers1 = { 12, 241, 4, 53, 7, 9 };
+//int count = 0;
+//for (int i = 0; i < numbers1.Length; i++)
+//{
+//    if (numbers1[i] < 10 && numbers1[i] >= 0)
+//    {
+//        count++;
+//    }
+//}
+//Console.WriteLine(count);
+#endregion
